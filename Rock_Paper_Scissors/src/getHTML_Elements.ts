084fbc4b4cdcc2dec546ac5ed979e107bcae4684
Amme <anne-sophie.nehls@t-online.de>
2024-08-22 15:13:26 +0200

    Button+A-Tag

--- Rock_Paper_Scissors/src/getHTML_Elements.ts
+++ Rock_Paper_Scissors/src/getHTML_Elements.ts
@@ -1,1 +1,28 @@
-const Schere = document.getElementById("") as HTMLButtonElement
+import Choise from "./Enum/Choise"
+//# Buttons
+export const scissor = document.getElementById("scissor") as HTMLButtonElement
+export const stone = document.getElementById("rock") as HTMLButtonElement
+export const paper = document.getElementById("paper") as HTMLButtonElement
+
+scissor?.addEventListener("click", (event: Event)=>{
+    event.preventDefault
+    const playerValue = Choise.Scissors
+    return playerValue
+})
+
+stone?.addEventListener("click", (event: Event)=>{
+    event.preventDefault
+    const playerValue = Choise.Rock
+    return playerValue
+})
+
+paper?.addEventListener("click", (event: Event)=>{
+    event.preventDefault
+    const playerValue = Choise.Paper
+    return playerValue
+})
+
+//# retrun
+const restart = document.getElementById("restart") as HTMLElement
+
+restart.addEventListener("click",()=>{})
